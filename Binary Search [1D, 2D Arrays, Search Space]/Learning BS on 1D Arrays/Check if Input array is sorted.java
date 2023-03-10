0cf6906b09e8e1f1851e588c93e5a37fe303b9f3
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2023-03-10 20:00:54 +0530

    Create Check if Input array is sorted.java

---
+++ Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/Check if Input array is sorted.java
@@ -0,0 +1,13 @@
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        boolean flag=true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
