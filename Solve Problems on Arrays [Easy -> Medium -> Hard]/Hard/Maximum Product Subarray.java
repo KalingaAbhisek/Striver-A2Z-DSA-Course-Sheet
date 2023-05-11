a201f15970091c74afbc359592fe575a96fcbefa
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2023-05-11 07:54:47 +0530

    Create Maximum Product Subarray.java

---
+++ Solve Problems on Arrays [Easy -> Medium -> Hard]/Hard/Maximum Product Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int pref=1;
+        int suff=1;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(pref==0)
+                pref=1;
+            if(suff==0)
+                suff=1;
+            pref*=nums[i];
+            suff*=nums[nums.length-i-1];
+            max=Math.max(max,Math.max(pref,suff));
+        }
+        return max;
+    }
+}
